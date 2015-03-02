165e1c388c56b575ae015d810f2a71105a6a5f75
awaelchli <adrian.waelchli@students.unibe.ch>
2015-03-03 00:18:45 +0100

    clean up

--- run.m
+++ run.m
@@ -1,6 +1,8 @@
 clear;
 %% Parameters
-%path = 'lightFields/messerschmitt/7x7x384x512/';
+% Note: paths are relative to the current folder
+%
+% path = 'lightFields/messerschmitt/7x7x384x512/';
 path = 'lightFields/dice/';
 imageType = 'png';
 resolution = [7, 7, 384, 512];          % Light field resolution
@@ -12,6 +14,7 @@ layerW = 100;                           % Width and height of layers in mm
 layerH = layerW * (resolution(3) / resolution(4));
 layerSize = [layerW, layerH];
 height = (Nlayers - 1) * layerDist;     % Height of layer stack
+iterations = 10;                        % Maximum number of iterations in optimization process
 outFolder = 'output/';                  % Output folder to store the layers
 
 %% Loading the light field
@@ -20,6 +23,11 @@ lightField = loadLightField(path, imageType, [resolution 3]);
 % lightField = lightField(3:5, 3:5, :, :, :);
 % r = size(lightField);
 % resolution = r(1:4);
+
+% convert the 4D light field to a matrix of size [ prod(resolution), 3 ],
+% and each column of this matrix represents a color channel of the light
+% field
+lightFieldVector = reshape(permute(lightField, [4, 3, 2, 1, 5]), [], 3);
 
 %% Computing index arrays for sparse matrix P
 % upper bound for number of non-zero values in the matrix P
@@ -32,17 +40,14 @@ S = ones(maxNonZeros, 1);       % values
 % index of the current non-zero element used in the for loop below.
 index = 0;
 
-lightFieldVector = zeros(prod(resolution), 3);
-
 fprintf('Computing matrix P...\n');
 tic;
+
 for imageX = 1 : resolution(2)
     for imageY = 1 : resolution(1)
         
-        % compute angles for incoming rays from current view
+        % compute relative angles for incoming rays from current view
         [angleX, angleY] = computeRayAngles(imageX, imageY, fov, resolution([2, 1]));
-        fprintf('angleX: %i, ', angleX);
-        fprintf('angleY: %i \n', angleY);
         
         for pixelX = 1 : resolution(4)
             for pixelY = 1 : resolution(3)
@@ -53,8 +58,6 @@ for imageX = 1 : resolution(2)
 %                 row = sub2ind(resolution, imageY, imageX, pixelY, pixelX);
                 row = ((imageY - 1) * resolution(2) + imageX - 1) * resolution(3) * resolution(4) + ...
                        (pixelY - 1) * resolution(4) + pixelX;
-                   
-                lightFieldVector(row, :) = lightField(imageY, imageX, pixelY, pixelX, :);
                 
                 [u, v] = pixelToSpaceCoordinates(pixelX, pixelY, resolution([4, 3]), layerSize);
                 
@@ -67,15 +70,13 @@ for imageX = 1 : resolution(2)
                     % check if intersection is out of bounds
                     if( all(intersection >= 0) && all(intersection < layerSize) )
                         % ray intersects with this layer
-%                         fprintf('hit\n');
+
 %                         col = sub2ind([Nlayers resolution([3, 4])], layer, intersectionP(2) + 1, intersectionP(1) + 1);
                         col = (layer - 1) * resolution(3) * resolution(4) + (intersectionP(2) - 1) * resolution(4) + intersectionP(1);
                         
                         index = index + 1;
                         I(index) = row;
                         J(index) = col;
-%                     else
-%                         fprintf('miss\n');
                     end
                 end
             end
@@ -88,19 +89,7 @@ P = sparse(I(1:index), J(1:index), S(1:index), prod(resolution), prod([Nlayers r
 save('P.mat', 'P');
 fprintf('Done calculating P. Calculation took %i minutes.\n', floor(toc / 60));
 
-%% Convert to log light field and separate rgb channels
-% lightField(lightField < 0.01) = 0.01;
-% lightField = log(lightField);
-% 
-% % RGB components of light field
-% Lr = lightField(:, :, :, :, 1);
-% Lg = lightField(:, :, :, :, 2);
-% Lb = lightField(:, :, :, :, 3);
-% 
-% % Convert the light field to a column vector for each channel
-% Lr = reshape(Lr, prod(resolution), 1);
-% Lg = reshape(Lg, prod(resolution), 1);
-% Lb = reshape(Lb, prod(resolution), 1);
+%% Convert to log light field
 
 lightFieldVector(lightFieldVector < 0.01) = 0.01;
 lightFieldVector = log(lightFieldVector);
@@ -111,21 +100,18 @@ lb = zeros(size(P, 2), 1) + log(0.01);
 ub = zeros(size(P, 2), 1); 
 % lb = zeros(size(P, 2), 1);
 % ub = ones(size(P, 2), 1);
-options = optimset('Display', 'iter', 'MaxIter', 10);
+options = optimset('Display', 'iter', 'MaxIter', iterations);
 
+fprintf('Running optimization for red color channel...\n');
 layersR = lsqlin(P, lightFieldVector(:, 1), [], [], [], [], lb, ub, [], options);
+
+fprintf('Running optimization for green color channel...\n');
 layersG = lsqlin(P, lightFieldVector(:, 2), [], [], [], [], lb, ub, [], options);
+
+fprintf('Running optimization for blue color channel...\n');
 layersB = lsqlin(P, lightFieldVector(:, 3), [], [], [], [], lb, ub, [], options);
 
-% layersR = lsqlin(P, Lr, [], [], [], [], lb, ub, [], options);
-% layersG = lsqlin(P, Lg, [], [], [], [], lb, ub, [], options);
-% layersB = lsqlin(P, Lb, [], [], [], [], lb, ub, [], options);
-
 fprintf('Optimization took %i minutes.\n', floor(toc / 60));
-% 
-% layersR = reshape(layersR, Nlayers, resolution(3), resolution(4));
-% layersG = reshape(layersG, Nlayers, resolution(3), resolution(4));
-% layersB = reshape(layersB, Nlayers, resolution(3), resolution(4));
 
 %% Extract layers from optimization
 
@@ -133,32 +119,25 @@ layersR = exp(layersR);
 layersG = exp(layersG);
 layersB = exp(layersB);
 
-layers = zeros(Nlayers, resolution(3), resolution(4), 3);
-
-for layer = 1 : Nlayers
-    for y = 1 : resolution(3)
-        for x = 1 : resolution(4)
-            % convert layer-pixel coordinates to linear index
-            idx = (layer - 1) * resolution(3) * resolution(4) + (y - 1) * resolution(4) + x;
-            r = layersR(idx);
-            g = layersG(idx);
-            b = layersB(idx);
-            layers(layer, y, x, :) = [r g b];
-        end
-    end
-end
+% convert the layers from column vector to a matrix of dimension [Nlayers, height, width, channel]
+layers = cat(2, layersR, layersG, layersB);
+layers = permute(reshape(layers, resolution(4), resolution(3), Nlayers, 3), [3, 2, 1, 4]);
 
 %% Save and display each layer
 close all;
 
 layerSize = [180, 180 * (resolution(3) / resolution(4))];
 
+if(exist(outFolder, 'dir'))
+    rmdir(outFolder, 's');
+end
+mkdir(outFolder);
+
 for layer = 1 : Nlayers
-%     r = squeeze(layersR(layer, :, :));
-%     g = squeeze(layersG(layer, :, :));
-%     b = squeeze(layersB(layer, :, :));
-%     im = cat(3, r, g, b);
+
+    % current image of layer
     im = cat(3, squeeze(layers(layer, :, :, :)));
+    
     % add padding to image
     padding = 20;
     im = padarray(im, [padding, padding], 1);
@@ -177,9 +156,7 @@ for layer = 1 : Nlayers
     im = insertText(im, [w - offset h - offset], layer, ...
                     'AnchorPoint', 'Center', 'BoxOpacity', 0);
     % save images and print to pdf
-    if(~exist(outFolder, 'dir'))
-        mkdir(outFolder);
-    end
+    
     imwrite(im, [outFolder num2str(layer) '.png']);
     printToPDF(im, printSize, [outFolder num2str(layer) '.pdf']);
 end
