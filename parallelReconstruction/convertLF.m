964e229f9ae238a328a07126f4e3bc0bfeabdbfc
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-04-16 17:44:02 +0200

    :)

--- parallelReconstruction/convertLF.m
+++ parallelReconstruction/convertLF.m
@@ -1,15 +1,11 @@
 % Convert a light field
 % All sizes are in millimeters 
 
-resVirtualPlane = resolution([3, 4]);
-% layerRes = [96, 110];
+resVirtualPlane = resolution([1, 2]);
 % Dimension of the camera plane
 cPlane = cameraDist .* (resolution([1, 2]) - 1);
 % Dimension of the virtual plane
 vPlane = [1000, 1000];
-
-dimension = [resolution(3), resolution(4), resVirtualPlane(1), resVirtualPlane(2), channels];
-
 
 % Camera indices
 CindY = zeros(resolution(3), resVirtualPlane(1));
@@ -24,7 +20,7 @@ originShift = - vPlane([2, 1]) / 2;
 
 % All possible angles
 % Each angle is a relative angle, the tangent of the actual angle
-[anglesX, anglesY] = computeRayAngles(fov, resVirtualPlane);
+[anglesX, anglesY] = computeRayAngles(fov, resolution([3, 4]));
 
 anglesY = -anglesY;
 anglesX = -anglesX;
@@ -55,15 +51,29 @@ CindX = CindX + cPlane(2) / 2;
 % cY = (CindY - min(CindY(:))) / (max(CindY(:)) - min(CindY(:)));
 % imshow(cY);
 
-
-CindY(CindY < 0) = 0;
-CindY(CindY > cPlane(1)) = 0;
-
-CindX(CindX < 0) = 0;
-CindX(CindX > cPlane(2)) = 0;
+%  
+% CindY(CindY < 0) = 0;
+% CindY(CindY > cPlane(1)) = 0;
+% 
+% CindX(CindX < 0) = 0;
+% CindX(CindX > cPlane(2)) = 0;
 
 cY = (CindY - min(CindY(:))) / (max(CindY(:)) - min(CindY(:)));
 imshow(cY);
 
+shiftY = anglesY * -planeDist;
+shiftX = anglesX * -planeDist;
 
-LF = interpn(lightField, CindY, CindX, PindY, PindX);
+for iAngleY = 1 : resolution(3)
+    for iAngleX = 1 : resolution(4)
+        
+        PindY(iAngleY, :) = posY + shiftY(iAngleY);
+        PindX(iAngleX, :) = posX + shiftX(iAngleX);
+        
+    end
+end
+
+PindY = -PindY + cPlane(1) / 2;
+PindX = PindX + cPlane(2) / 2;
+
+LF = interpn(lightField(:, :, :, :, 1), CindY(:), CindX(:), PindY(:), PindX(:));
