819d14c44ba4c1c0ad763b378de9cfe1bf1cb8c0
awaelchli <adrian.waelchli@students.unibe.ch>
2015-04-30 00:04:42 +0200

    renaming, documenting, cleaning up

--- rotatedReconstruction/computeMatrixP.m
+++ rotatedReconstruction/computeMatrixP.m
@@ -2,23 +2,25 @@ function [ P ] = computeMatrixP( Nlayers, ...
                                  resolution, ...
                                  layerResolution, ...
                                  layerSize, ...
-                                 originLayers, ...
                                  fov, ...
-                                 layerDist, ...
+                                 distanceBetweenLayers, ...
                                  cameraPlaneDistance, ...
                                  distanceBetweenTwoCameras, ...
                                  focalLength)
 % Inputs:
 %
-%   Nlayers:        The number of layers in the attenuator
-%   resolution:     The resolution of the light field [viewsY, viewsX, pixelsY, pixelsX]
-%   layersSize:     The size of the layer in millimeters [width, height]
-%   originLF:       The origin of the light field relative to the origin of
-%                   the attenuator
-%   originLayers:   The origin of the attenuator
-%   fov:            The field of view [fovX, fovY] in Y and X direction of 
-%                   the light field (not the fov of the cameras)
-%   layerDist:      The distance between the layers
+%   Nlayers:                        Number of layers in the attenuator
+%   resolution:                     The resolution of the light field
+%                                   [viewsY, viewsX, pixelsY, pixelsX]
+%   layersSize:                     Size of the layer in mm [width, height]
+%   fov:                            The field of view [fovX, fovY] in X and
+%                                   Y direction of the cameras in radians
+%   distanceBetweenLayers:          Distance between the layers in mm
+%   cameraPlaneDistance:            Distance between the camera plane and
+%                                   the origin of the scene in mm
+%   distanceBetweenTwoCameras:      distance between two cameras on the
+%                                   camera plane in mm
+%   focalLength:                    focal length of the cameras in mm
 %
 % Output:
 %
@@ -35,46 +37,25 @@ S = ones(maxNonZeros, 1);       % values
 % index of the current non-zero element used in the for loop below.
 c = 1;
 
-% % 2D pixel positions (relative to one layer) in coordinates of the light field
-% [posX, posY] = pixelToSpaceCoordinates(resolution([4, 3]), layerSize, originLF);
-% % The scale is 1 / pixelSize, it is used to go from space coordinates back to
-% % pixel indices
-% scale = resolution([4, 3]) ./ layerSize;
-
 [ cameraPositionMatrixY, cameraPositionMatrixX ] = computeCameraPositions(resolution([1, 2]), ...
                                                                           distanceBetweenTwoCameras([2, 1]));
 
 [ layerPositionMatrixY, layerPositionMatrixX ] = computePixelPositionsOnLayer(layerResolution, ...
                                                                               layerSize([2, 1]));
-% TODO : check if correct
-layerPositionsZ = -(Nlayers - 1) * layerDist/2:layerDist:(Nlayers - 1) * layerDist/2;
-% layerPositionsZ = (Nlayers - 1) * layerDist : -layerDist : 0;
-% layerPositionsZ = layerPositionsZ + originLayers(3);
 
-for imageX = 1 : resolution(2)
-    for imageY = 1 : resolution(1)
-        
-        % compute relative angles for incoming rays from current view
-        %[angleX, angleY] = computeRayAngles(imageX, imageY, fov, resolution([2, 1]));
-       
-        % intersection points of rays with relative angles [angleX, angleY]
-        % on the first layer (most bottom layer), can go outside of layer
-        % boudaries
-%         posXL1 = posX + (originLayers(3) - originLF(3)) * angleX;
-%         posYL1 = posY + (originLayers(3) - originLF(3)) * angleY;
-        cameraPosition = [ cameraPositionMatrixY(imageY, imageX), ...
-                           cameraPositionMatrixX(imageY, imageX) ];
+layerPositionsZ = -(Nlayers - 1) * distanceBetweenLayers / 2 : distanceBetweenLayers : (Nlayers - 1) * distanceBetweenLayers / 2;
+
+for camIndexX = 1 : resolution(2)
+    for camIndexY = 1 : resolution(1)
+    
+        % get the position of the current camera on the camera plane
+        cameraPosition = [ cameraPositionMatrixY(camIndexY, camIndexX), ...
+                           cameraPositionMatrixX(camIndexY, camIndexX) ];
         
         for layer = 1 : Nlayers
             
-            % shift intersection points according to current layer
-%             posXCurrentLayer = posXL1 - (layer - 1) * layerDist * angleX;
-%             posYCurrentLayer = posYL1 - (layer - 1) * layerDist * angleY;
-%             
-%             % pixel indices 
-%             pixelsX = ceil(scale(1) * (posXCurrentLayer - originLayers(1)));
-%             pixelsY = ceil(scale(2) * (posYCurrentLayer - originLayers(2)));
-
+            % adjust distance for current layer; the coordinate origin is
+            % at the center of the layer stack
             distanceBetweenCameraPlaneAndLayer = cameraPlaneDistance + layerPositionsZ(layer);
             
             % computing the relative location of the intersecting rays
@@ -86,7 +67,7 @@ for imageX = 1 : resolution(2)
                                                                distanceBetweenCameraPlaneAndLayer, ...
                                                                layerPositionMatrixY, ...
                                                                layerPositionMatrixX);
-            % converting the metric location to pixel indecies
+            % converting the metric positions to pixel indicies
             [ pixelIndexMatrixY, ...
               pixelIndexMatrixX ] = computePixelIndicesForCamera( ...
                                                                pixelPositionMatrixY, ...
@@ -95,8 +76,7 @@ for imageX = 1 : resolution(2)
                                                                fov([2, 1]), ...
                                                                resolution([3, 4]));
 
-%             size(pixelIndexMatrixY)
-%             size(pixelIndexMatrixX)
+
             layerPixelIndicesY = find(pixelIndexMatrixY(:, 1)); % col vector
             layerPixelIndicesX = find(pixelIndexMatrixX(1, :)); % row vector
             
@@ -113,25 +93,13 @@ for imageX = 1 : resolution(2)
             
             cameraPixelIndicesY = pixelIndexMatrixY(pixelIndexMatrixY(:, 1) ~= 0, 1); % col vector
             cameraPixelIndicesX = pixelIndexMatrixX(1, pixelIndexMatrixX(1, :) ~= 0); % row vector
-            
-%             size(cameraPixelIndicesY)
-%             size(cameraPixelIndicesX)
-            
+
             cameraPixelIndicesY = repmat(cameraPixelIndicesY, 1, numel(cameraPixelIndicesX)); 
             cameraPixelIndicesX = repmat(cameraPixelIndicesX, size(cameraPixelIndicesY, 1), 1); 
-            
-%             cameraPixelIndicesY
-%             cameraPixelIndicesX
-            
-%             size(cameraPixelIndicesY)
-%             size(cameraPixelIndicesX)
 
             % make copies of the image indices
-            imageIndicesY = imageY + zeros(size(cameraPixelIndicesY));
-            imageIndicesX = imageX + zeros(size(cameraPixelIndicesX));
-            
-%             cameraPixelIndicesY = cameraPixelIndicesY';
-%             cameraPixelIndicesX = cameraPixelIndicesX';
+            imageIndicesY = camIndexY + zeros(size(cameraPixelIndicesY));
+            imageIndicesX = camIndexX + zeros(size(cameraPixelIndicesX));
             
             % convert the 4D subscipts to row indices all at once
             rows = sub2ind(resolution, imageIndicesY(:), imageIndicesX(:), cameraPixelIndicesY(:), cameraPixelIndicesX(:));
