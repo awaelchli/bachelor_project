6088fa036c25440db4759b4da7fc714f3378bf06
awaelchli <adrian.waelchli@students.unibe.ch>
2015-05-03 01:50:37 +0200

    bug fixing

--- rotatedReconstruction/computePixelIndicesForCamera.m
+++ rotatedReconstruction/computePixelIndicesForCamera.m
@@ -28,18 +28,31 @@ pixelPositionMatrixX = pixelPositionMatrixX + maxPositionX;
 
 % pixelPositionMatrixX
 
-% Scale to pixel indices
+pixelPositionMatrixY(pixelPositionMatrixY < 0) = 0;
+pixelPositionMatrixX(pixelPositionMatrixX < 0) = 0;
+pixelPositionMatrixY(pixelPositionMatrixY > sizeOfView(1)) = 0;
+pixelPositionMatrixX(pixelPositionMatrixX > sizeOfView(2)) = 0;
+
+% pixelPositionMatrixX
+
+% Scale positions to the range [0, resolution - 1]
 pixelPositionMatrixY = scalePositionToIndex(1) .* pixelPositionMatrixY;
 pixelPositionMatrixX = scalePositionToIndex(2) .* pixelPositionMatrixX;
+
+% Add one to the valid coordinates so that they are in range of [1, resolution]
+pixelPositionMatrixY(pixelPositionMatrixY ~= 0) = pixelPositionMatrixY(pixelPositionMatrixY ~= 0) + 1;
+pixelPositionMatrixX(pixelPositionMatrixX ~= 0) = pixelPositionMatrixX(pixelPositionMatrixX ~= 0) + 1;
+
+% pixelPositionMatrixX
 
 pixelPositionMatrixY = floorCeilOrRoundHandle(pixelPositionMatrixY);
 pixelPositionMatrixX = floorCeilOrRoundHandle(pixelPositionMatrixX);
 
 % set all positions that fall out as 'invalid'
-pixelPositionMatrixY(pixelPositionMatrixY < 1) = 0;
-pixelPositionMatrixX(pixelPositionMatrixX < 1) = 0;
-pixelPositionMatrixY(pixelPositionMatrixY > cameraResolution(1)) = 0;
-pixelPositionMatrixX(pixelPositionMatrixX > cameraResolution(2)) = 0;
+% pixelPositionMatrixY(pixelPositionMatrixY < 1) = 0;
+% pixelPositionMatrixX(pixelPositionMatrixX < 1) = 0;
+% pixelPositionMatrixY(pixelPositionMatrixY > cameraResolution(1)) = 0;
+% pixelPositionMatrixX(pixelPositionMatrixX > cameraResolution(2)) = 0;
 
 pixelIndexMatrixY = pixelPositionMatrixY;
 pixelIndexMatrixX = pixelPositionMatrixX;
