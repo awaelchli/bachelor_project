8859ed907a6475e2b9e1cc6a5d314701deb39e93
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-05-06 17:10:19 +0200

    cosmetic changes

--- rotatedReconstruction/computePixelPositionsOnSensorPlaneRelativeToCamera.m
+++ rotatedReconstruction/computePixelPositionsOnSensorPlaneRelativeToCamera.m
@@ -8,8 +8,8 @@ function [ pixelPositionMatrixY, ...
 % lengths in mm
 
 % Shift positions so that they are relative to the camera
-positionMatrixYRelativeToCam = -(cameraPosition(1) - layerPositionMatrixY); 
-positionMatrixXRelativeToCam = -(cameraPosition(2) - layerPositionMatrixX); 
+positionMatrixYRelativeToCam = layerPositionMatrixY - cameraPosition(1);
+positionMatrixXRelativeToCam = layerPositionMatrixX - cameraPosition(2); 
 
 % Compute the positions on the sensor plane of the camera
 pixelPositionMatrixY = positionMatrixYRelativeToCam .* sensorPlaneDist ./ distanceBetweenCameraPlaneAndLayer;
