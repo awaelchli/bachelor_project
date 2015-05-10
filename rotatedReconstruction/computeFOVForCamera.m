3ff2d2fbb3e41a9ba1a7bcac30dbb5b72ce8004d
awaelchli <adrian.waelchli@students.unibe.ch>
2015-05-10 17:41:44 +0200

    helper methods added

--- rotatedReconstruction/computeFOVForCamera.m
+++ rotatedReconstruction/computeFOVForCamera.m
@@ -1,6 +1,8 @@
 function [ fov ] = computeFOVForCamera( distanceCameraPlaneToSensorPlane, ...
-                                                aspectRatio )
-% fov = [fovHorizontal, fovVertical]
+                                        aspectRatio )
+% Computes the horizontal and vertical field of view of the camera given
+% the distance of the center of projection to the sensor plane and the
+% aspect ratio (width / height).
 
 fovHorizontal = 2 * atan(0.5 / distanceCameraPlaneToSensorPlane);
 fovVertical = fovHorizontal / aspectRatio;
