fcc6c5df680c5038b0e74b5ffab850d9c4c05231
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-05-13 16:48:32 +0200

    downsampling a light field

---
+++ parallelReconstruction/downsampleLighField.m
@@ -0,0 +1,17 @@
+function [ sampledLightField ] = downsampleLighField( originalLightField, scaleFactor )
+
+originalResolution = size(originalLightField);
+newResolution = originalResolution;
+newResolution([3, 4]) = ceil(scaleFactor * originalResolution([3, 4]));
+
+sampledLightField = zeros(newResolution);
+
+for y = 1 : originalResolution(1)
+    for x = 1 : originalResolution(2)
+        sampledLightField(y, x, :, :, :) = imresize(squeeze(originalLightField(y, x, :, :, :)), scaleFactor);
+    end
+end
+
+
+end
+
