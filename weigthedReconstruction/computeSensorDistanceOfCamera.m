ec953f0975e1366e423ac3b903972687927b1cab
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-05-27 09:49:12 +0200

    remove some duplicate/unused function

--- weigthedReconstruction/computeSensorDistanceOfCamera.m
+++
@@ -1,9 +0,0 @@
-function [ sensorDistance ] = computeSensorDistanceOfCamera( fov )
-% Computes the distance between the sensor and the center of projection of
-% a camera, given the field of view [fovHorizontal, fovVertical] in
-% horizontal and vertical direction (in radians). 
-
-sensorDistance =  1 / (2 * tan(fov(1) / 2));
-
-end
-
