ac5e6e538ca65e3f5196f6974cfa6509455b5fb1
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-06-03 13:09:45 +0200

    fix for negative weights

--- weigthedReconstruction/tentWeightFunction.m
+++ weigthedReconstruction/tentWeightFunction.m
@@ -3,7 +3,8 @@ function [ weights ] = tentWeightFunction( data, maxY, maxX )
 d = sqrt(data(:, 1).^2 + data(:, 2).^2);
 slope = maxY / maxX;
 
-weights = 1 - slope * d;
+weights = maxY - slope * d;
+weights(weights < 0) = 0;
 
 end
 
