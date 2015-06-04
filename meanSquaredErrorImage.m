ad17301cb991789fc26d41ad248e712f49be7c35
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-06-04 15:41:56 +0200

    renaming

--- meanSquaredErrorImage.m
+++ meanSquaredErrorImage.m
@@ -1,9 +1,10 @@
-function [ errorImage, mse ] = meanSquaredErrorImage( image1, image2 )
+function [ errorImage, rmse ] = meanSquaredErrorImage( image1, image2 )
 
-errorImage = 255*255*(image1 - image2).^2;
+errorImage = 255 * 255 * (image1 - image2) .^ 2;
 errorImage = sum(errorImage, 3);
 
-mse = sqrt(mean(errorImage(:)));
-errorImage = errorImage/255/255;
+rmse = sqrt(mean(errorImage(:)));
+errorImage = errorImage / 255 / 255;
+
 end
 
