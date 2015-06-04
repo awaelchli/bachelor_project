27ab8bac1947c536602a1d8262b55f7383d3d674
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-06-04 15:06:44 +0200

    proper naming

---
+++ rootMeanSquaredErrorImage.m
@@ -0,0 +1,10 @@
+function [ errorImage, rmse ] = rootMeanSquaredErrorImage( image1, image2 )
+
+errorImage = 255 * 255 * (image1 - image2) .^ 2;
+errorImage = sum(errorImage, 3);
+
+rmse = sqrt(mean(errorImage(:)));
+errorImage = errorImage / 255 / 255;
+
+end
+
