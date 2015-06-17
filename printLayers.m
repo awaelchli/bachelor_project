8f3bef3996e1b12138cc7f358ade3baf604a5fa1
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-06-17 11:50:14 +0200

    improved code, printing monochrome layers supported

--- printLayers.m
+++ printLayers.m
@@ -7,21 +7,18 @@ function printLayers( layers, layerSize, outFolder, filename, count )
 %                   .pdf extension)
 
 padding = floor(size(layers, 1) / 15);
-resolution = [size(layers, 1), size(layers, 2)];
-h = resolution(1) + 2 * padding;
-w = resolution(2) + 2 * padding;
-Nlayers = size(layers, 3);
-channels = size(layers, 4);
+originalLayerResolution = [size(layers, 1), size(layers, 2)];
+NumberOfLayers = size(layers, 3);
 
-images = zeros(h, w, channels, Nlayers);
+layers = permute(layers, [1, 2, 4, 3]);
+layers = padarray(layers, [padding, padding, 0, 0], 1);
 
-for layer = 1 : Nlayers
-    % current image of layer
-    im = cat(3, squeeze(layers(:, :, layer, :)));
+newResolution = [size(layers, 1), size(layers, 2)];
+
+for layer = 1 : NumberOfLayers
     
-    % add padding to image
-    im = padarray(im, [padding, padding], 1);
-    
+    currentLayer = layers(:, :, :, layer);
+
     % insert markers that help for alignment
     offset = floor(padding / 2);
 %     pos = [offset offset;
@@ -30,35 +27,36 @@ for layer = 1 : Nlayers
 %     im = insertMarker(im, pos, 'Color', 'Black', 'Size', offset);
     
     % Insert layer number into image
-    im = insertTextIntoImage(im, num2str(count), [w - offset, h - offset], 16);
+    currentLayer = insertTextIntoImage(currentLayer, num2str(count), newResolution([2, 1]) - offset, 16);
+    
     % Save image of layer
-    imwrite(im, [outFolder num2str(count) '.png']);
+    imwrite(currentLayer, [outFolder num2str(count) '.png']);
     
     count = count + 1;
-    images(:, :, :, layer) = im;
+    layers(:, :, :, layer) = currentLayer;
 end
 
 
 fig = figure('Menubar', 'none', 'Visible', 'off');
 
-for layer = 1 : Nlayers
-    img = images(:, :, :, layer);
+for layer = 1 : NumberOfLayers
+    img = layers(:, :, :, layer);
     
-    relPosY = (layer - 1) / Nlayers;
-    relSize = 1 / Nlayers;
+    relPosY = (layer - 1) / NumberOfLayers;
+    relSize = 1 / NumberOfLayers;
     subplot('Position', [0, relPosY, relSize, relSize]), image(im2uint8(img))
     
     set(gca, 'XTickLabel', [], 'YTickLabel', [])
     set(gca, 'XTick', [], 'YTick', [])
 end
 
-pixelSize = layerSize ./ [resolution(2) resolution(1)];
+pixelSize = layerSize ./ [originalLayerResolution(2) originalLayerResolution(1)];
 padSize = pixelSize .* padding;
 printSize = layerSize + 2 .* padSize;
 
 set(fig, 'PaperPositionMode', 'manual')
 set(fig, 'PaperUnits', 'centimeters')
-set(fig, 'PaperPosition',[1, 1, printSize(1) * Nlayers / 10,  printSize(2) * Nlayers / 10])
+set(fig, 'PaperPosition',[1, 1, printSize(1) * NumberOfLayers / 10,  printSize(2) * NumberOfLayers / 10])
 set(fig, 'PaperType', 'A4')
 set(fig, 'PaperOrientation', 'portrait')
 
@@ -77,6 +75,6 @@ imageWithText = hFrame.cdata;
 
 close(f);
 
-imageWithText = imageWithText(1 : size(image, 1), 1 : size(image, 2), :);
+imageWithText = imageWithText(1 : size(image, 1), 1 : size(image, 2), 1 : size(image, 3));
 
 end
