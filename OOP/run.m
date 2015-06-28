b4623fbcf583d9010e742e6a5bedf667ba46ab70
awaelchli <adrian.waelchli@students.unibe.ch>
2015-06-28 15:57:28 +0200

    script to test

---
+++ OOP/run.m
@@ -0,0 +1,12 @@
+
+editor = LightFieldEditor();
+editor.loadData('../lightFields/dice/perspective/3x3-.2_rect/', 'png', [3, 3], 1);
+editor.distanceBetweenTwoCameras = [0.2, 0.2];
+editor.cameraPlaneZ = 8;
+editor.sensorSize = [4, 4 * 1.3584];
+
+lightField = editor.getLightField();
+attenuator = Attenuator(5, lightField.spatialResolution, lightField.sensorPlane.planeSize, 1, lightField.channels);
+
+rec = Reconstruction(lightField, attenuator);
+rec.computeLayers();
