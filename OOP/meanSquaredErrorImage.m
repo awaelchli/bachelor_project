425c37807ec43b30ebc1120008167dc290ff21d8
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-07-03 11:23:34 +0200

    temporarily added

---
+++ OOP/meanSquaredErrorImage.m
@@ -0,0 +1,9 @@
+function [ errorImage, rmse ] = meanSquaredErrorImage( image1, image2 )
+
+errorImage = 255^2 * (image1 - image2) .^ 2;
+errorImage = sum(errorImage, 3);
+
+rmse = sqrt(mean(errorImage(:)));
+errorImage = errorImage / (255^2);
+
+end
