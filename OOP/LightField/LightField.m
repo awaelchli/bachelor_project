805b66cc2045a9ebb0998ce489c24fc244ac0d93
awaelchli <adrian.waelchli@students.unibe.ch>
2015-07-06 13:57:22 +0200

    Make a general light field super class for all types of light fields

--- OOP/LightField/LightField.m
+++ OOP/LightField/LightField.m
@@ -1,62 +1,27 @@
-classdef LightField < handle
-    %LIGHTFIELD Summary of this class goes here
-    %   Detailed explanation goes here
-    
-    properties (Constant)
-        lightFieldDimension = 4;
-        angularDimensions = [1, 2];
-        spatialDimensions = [3, 4];
-        channelDimension = 5;
-    end
+classdef LightField < AbstractLightField
     
     properties (SetAccess = private)
-        lightFieldData;
         cameraPlane;
         sensorPlane;
     end
     
     properties (Dependent, SetAccess = private)
-        resolution;
-        angularResolution;
-        spatialResolution;
-        channels;
         distanceCameraToSensorPlane;
     end
     
     methods
         
         function this = LightField(lightFieldData, cameraPlane, sensorPlane)
+            this = this@AbstractLightField(lightFieldData);
             % TODO: write invariant to check if resolution of
             % lightFieldData corrensponds to resolution of cameraPlane and
             % sensorPlane
-            this.lightFieldData = lightFieldData;
             this.cameraPlane = cameraPlane;
             this.sensorPlane = sensorPlane;
         end
         
-        function resolution = get.resolution(this)
-            resolution = size(this.lightFieldData);
-            resolution = resolution([LightField.angularDimensions, LightField.spatialDimensions]);
-        end
-        
-        function angularResolution = get.angularResolution(this)
-            angularResolution = this.resolution(LightField.angularDimensions);
-        end
-        
-        function spatialResolution = get.spatialResolution(this)
-            spatialResolution = this.resolution(LightField.spatialDimensions);
-        end
-        
-        function channels = get.channels(this)
-            channels = size(this.lightFieldData, LightField.channelDimension);
-        end
-        
         function distance = get.distanceCameraToSensorPlane(this)
             distance = abs(this.cameraPlane.z - this.sensorPlane.z);
-        end
-        
-        function replaceView(this, cameraIndexY, cameraIndexX, image)
-           this.lightFieldData(cameraIndexY, cameraIndexX, : , : , :) = image; 
         end
         
     end
