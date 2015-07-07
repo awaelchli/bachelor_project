67a7a3bcf5e107f6839c057be23bc75052fc67c4
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-07-07 17:52:30 +0200

    tying to adjust the resampling plane

---
+++ OOP/Reconstruction/ReconstructionForResampledLF_V2.m
@@ -0,0 +1,238 @@
+classdef ReconstructionForResampledLF_V2 < AbstractReconstruction
+    %RECONSTRUCTION Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (SetAccess = private)
+        resampledLightField;
+        resamplingPlane;
+    end
+    
+    properties
+        iterations = 20;
+    end
+    
+    methods
+        
+        function this = ReconstructionForResampledLF_V2(lightField, attenuator, resamplingPlane)
+            
+            this = this@AbstractReconstruction(lightField, attenuator);
+            
+            resampledLFResolution = [lightField.angularResolution, attenuator.planeResolution];
+            resampledLFData = zeros([resampledLFResolution, lightField.channels]);
+            newSensorPlane = SensorPlane(attenuator.planeResolution, lightField.sensorPlane.planeSize, lightField.sensorPlane.z);
+            this.resamplingPlane = resamplingPlane;
+            
+            % Initialize empty light fields
+            this.resampledLightField = LightFieldP(resampledLFData, lightField.cameraPlane, newSensorPlane);
+            reconstructedLightField = LightFieldP(resampledLFData, this.lightField.cameraPlane, newSensorPlane);
+            this.evaluation = ReconstructionEvaluation(this.resampledLightField, reconstructedLightField);
+            
+            this.propagationMatrix = PropagationMatrix(this.resampledLightField, attenuator);
+        end
+        
+    end
+    
+    methods (Access = protected)
+        
+        function runOptimization(this)
+            
+            P = this.propagationMatrix.formSparseMatrix();
+            
+            lightFieldVector = reshape(this.resampledLightField.lightFieldData, [], this.resampledLightField.channels);
+
+            % Convert to log light field
+            lightFieldVector(lightFieldVector < 0.01) = 0.01;
+            lightFieldVectorLogDomain = log(lightFieldVector);
+
+            % Optimization constraints
+            ub = zeros(this.propagationMatrix.size(2), this.resampledLightField.channels); 
+            lb = zeros(size(ub)) + log(0.01);
+            x0 = zeros(size(ub));
+            
+            % Solve using SART
+            attenuationValuesLogDomain = sart(P, lightFieldVectorLogDomain, x0, lb, ub, this.iterations);
+            
+            attenuationValues = exp(attenuationValuesLogDomain);
+            
+            attenuationValues = permute(attenuationValues, [2, 1]);
+            attenuationValues = reshape(attenuationValues, [this.attenuator.channels, this.attenuator.planeResolution, this.attenuator.numberOfLayers]);
+            attenuationValues = permute(attenuationValues, [4, 2, 3, 1]);
+
+            this.attenuator.attenuationValues = attenuationValues;
+        end
+        
+        function constructPropagationMatrix(this)
+            
+            layerResolution = this.attenuator.planeResolution;
+            angularResolution = this.lightField.angularResolution;
+
+            cameraPositionMatrixY = this.lightField.cameraPlane.cameraPositionMatrixY;
+            cameraPositionMatrixX = this.lightField.cameraPlane.cameraPositionMatrixX;
+        
+            pixelPositionsOnResamplingPlaneMatrixY = this.resamplingPlane.pixelPositionMatrixY;
+            pixelPositionsOnResamplingPlaneMatrixX = this.resamplingPlane.pixelPositionMatrixX;
+
+            [ pixelIndexOnResamplingPlaneMatrixY, pixelIndexOnResamplingPlaneMatrixX ] = ndgrid(1 : this.resamplingPlane.planeResolution(1), 1 : this.resamplingPlane.planeResolution(2));
+
+            fprintf('Views done: \n');
+
+%             for camIndexY = 1 : angularResolution(1)
+%                 for camIndexX = 1 : angularResolution(2)
+            for camIndexY = 1 : 1
+                for camIndexX = 1 : 1
+        
+                    % get the position of the current camera on the camera plane
+                    cameraPosition = [ cameraPositionMatrixY(camIndexY, camIndexX), ...
+                                       cameraPositionMatrixX(camIndexY, camIndexX), ...
+                                       this.lightField.cameraPlane.z ];
+        
+                    resamplingPlaneZ = this.resamplingPlane.z;
+
+                    [ positionsOnSensorPlaneMatrixY, ...
+                      positionsOnSensorPlaneMatrixX ] = this.projection(cameraPosition, ...
+                                                                        this.lightField.sensorPlane.z, ...
+                                                                        pixelPositionsOnResamplingPlaneMatrixY, ...
+                                                                        pixelPositionsOnResamplingPlaneMatrixX, ...
+                                                                        resamplingPlaneZ);
+                                                                    
+%                     positionsOnSensorPlaneMatrixY   
+%                     positionsOnSensorPlaneMatrixX
+                    
+                    [ sensorIntersectionMatrixY, ...
+                      sensorIntersectionMatrixX, validIndicesForSensor ] = this.lightField.sensorPlane.positionToPixelCoordinates(positionsOnSensorPlaneMatrixY, positionsOnSensorPlaneMatrixX);
+                  
+                  csum = sum(validIndicesForSensor, 1);
+                  rsum = sum(validIndicesForSensor, 2);
+%                   return;
+                  
+                  sensorIntersectionMatrixY(~rsum, :) = [];
+                  sensorIntersectionMatrixY(:, ~csum) = [];
+                  sensorIntersectionMatrixX(~rsum, :) = [];
+                  sensorIntersectionMatrixX(:, ~csum) = [];
+%                   validSensorIntersectionMatrixX = sensorIntersectionMatrixX(validIndicesForSensor);
+                  sensorIntersectionMatrixY
+%                  validSensorIntersectionMatrixY
+        
+%                     invalidRayIndicesForSensorY = sensorIntersectionMatrixY(:, 1) == 0;
+%                     invalidRayIndicesForSensorX = sensorIntersectionMatrixX(1, :) == 0;
+
+%                     pixelIndexOnSensorMatrixY = pixelIndexOnResamplingPlaneMatrixY;
+%                     pixelIndexOnSensorMatrixX = pixelIndexOnResamplingPlaneMatrixX;
+%                     
+%                     pixelIndicesOnSensorY = pixelIndexOnSensorMatrixY(validIndicesForSensor); % column vector
+%                     pixelIndicesOnSensorX = pixelIndexOnSensorMatrixX(validIndicesForSensor); % row vector
+
+                    % Interpolating the current view of the light field
+                    view = squeeze(this.lightField.lightFieldData(camIndexY, camIndexX, :, :, :));
+                    gridVectors = {sensorIntersectionMatrixY(:, 1), sensorIntersectionMatrixX(1, :), 1 : this.lightField.channels};
+
+                    % Remove arrays of singleton dimensions (2D light fields or single
+                    % channel)
+                    indicesOfScalars = cellfun(@isscalar, gridVectors);
+                    grid = cell(1, nnz(~indicesOfScalars));
+                    [ grid{:} ] = ndgrid(gridVectors{~indicesOfScalars});
+                    
+                   
+
+                    this.resampledLightField.replaceView(camIndexY, camIndexX, interpn(view, grid{:}));
+                    
+%                     this.propagationMatrix.submitEntries(camIndexY, camIndexX, ...
+%                                                          pixelIndicesOnSensorY, pixelIndicesOnSensorX, ...
+%                                                          1, ...
+%                                                          pixelIndexOnResamplingPlaneMatrixY, pixelIndexOnResamplingPlaneMatrixX, ...
+%                                                          ones(layerResolution));
+
+                    for layer = 1 : this.attenuator.numberOfLayers
+
+                        % adjust distance for current layer; the coordinate origin is
+                        % at the center of the layer stack
+                        currentLayerZ = this.attenuator.layerPositionZ(layer);
+                        
+                        [ positionsOnLayerMatrixY, ...
+                          positionsOnLayerMatrixX ] = this.projection(cameraPosition, ...
+                                                                      currentLayerZ, ...
+                                                                      pixelPositionsOnResamplingPlaneMatrixY, ...
+                                                                      pixelPositionsOnResamplingPlaneMatrixX, ...
+                                                                      resamplingPlaneZ);
+                        
+                        [ layerIntersectionMatrixY, ...
+                          layerIntersectionMatrixX ] = this.attenuator.positionToPixelCoordinates(positionsOnLayerMatrixY, positionsOnLayerMatrixX);
+                        
+                        pixelIndexOnLayerMatrixY = round(layerIntersectionMatrixY);
+                        pixelIndexOnLayerMatrixX = round(layerIntersectionMatrixX);
+                        
+                        weightsForLayerMatrix = this.computeRayIntersectionWeights(pixelIndexOnLayerMatrixY, ...
+                                                                                   pixelIndexOnLayerMatrixX, ...
+                                                                                   layerIntersectionMatrixY, ...
+                                                                                   layerIntersectionMatrixX);
+                                                                           
+                        invalidRayIndicesForLayerY = pixelIndexOnLayerMatrixY(:, 1) == 0;
+                        invalidRayIndicesForLayerX = pixelIndexOnLayerMatrixX(1, :) == 0;
+                        
+                        validRayIndicesY = ~(invalidRayIndicesForSensorY | invalidRayIndicesForLayerY);
+                        validRayIndicesX = ~(invalidRayIndicesForSensorX | invalidRayIndicesForLayerX);
+                        
+                        layerPixelIndicesY = pixelIndexOnLayerMatrixY(validRayIndicesY, validRayIndicesX); % column vector
+                        layerPixelIndicesX = pixelIndexOnLayerMatrixX(validRayIndicesY, validRayIndicesX); % row vector
+                        
+                        pixelIndicesOnSensorY = pixelIndexOnSensorMatrixY(validRayIndicesY, validRayIndicesX); % column vector
+                        pixelIndicesOnSensorX = pixelIndexOnSensorMatrixX(validRayIndicesY, validRayIndicesX); % row vector
+                        
+                        weightsForLayerMatrix = weightsForLayerMatrix(validRayIndicesY, validRayIndicesX);
+                        
+                        this.propagationMatrix.submitEntries(camIndexY, camIndexX, ...
+                                                             pixelIndicesOnSensorY, pixelIndicesOnSensorX, ...
+                                                             layer, ...
+                                                             layerPixelIndicesY, layerPixelIndicesX, ...
+                                                             weightsForLayerMatrix);
+                    end
+                    
+                    fprintf('(%i, %i) ', camIndexY, camIndexX);
+                end
+                
+                fprintf('\n');
+            end
+        end
+        
+        function [X, Y] = projection(~, centerOfProjection, targetPlaneZ, X, Y, Z)
+            
+            distanceBetweenCameraPlaneAndFirstPlane = centerOfProjection(3) - Z;
+            distanceBetweenCameraPlaneAndSecondPlane = centerOfProjection(3) - targetPlaneZ;
+       
+            % Shift positions to camera coordinate system
+            X = X - centerOfProjection(1);
+            Y = Y - centerOfProjection(2);
+
+            % Project positions from first plane to the target plane
+            X = X .* distanceBetweenCameraPlaneAndSecondPlane ./ distanceBetweenCameraPlaneAndFirstPlane;
+            Y = Y .* distanceBetweenCameraPlaneAndSecondPlane ./ distanceBetweenCameraPlaneAndFirstPlane;
+
+            % Shift positions back to world coordinate system
+            X = X + centerOfProjection(1);
+            Y = Y + centerOfProjection(2);
+            
+        end
+        
+        function weightMatrix = computeRayIntersectionWeights(this, ...
+                                                              pixelIndexMatrixY, ...
+                                                              pixelIndexMatrixX, ...
+                                                              intersectionMatrixY, ...
+                                                              intersectionMatrixX)
+
+            % Weights are computed based on the deviation from the exact pixel location
+            deviationY = intersectionMatrixY - pixelIndexMatrixY;
+            deviationX = intersectionMatrixX - pixelIndexMatrixX;
+
+            queryData = cat(3, deviationY, deviationX);
+            queryData = reshape(queryData, [], 2);
+
+            weightVector = this.weightFunctionHandle(queryData);
+            weightMatrix = reshape(weightVector, size(pixelIndexMatrixY));
+
+        end
+        
+    end
+
+
+end
+
--- OOP/run.m
+++ OOP/run.m
@@ -3,10 +3,10 @@ editor = LightFieldEditor();
 % editor.inputFromImageCollection('../lightFields/dice/perspective/3x3-.2_rect/', 'png', [3, 3], 1);
 editor.inputFromImageCollection('../lightFields/legotruck/', 'png', [17, 17], 0.3);
 % editor.inputFromImageCollection('../lightFields/tarot/small_angular_extent/', 'png', [17, 17], 0.2);
-editor.angularSliceY(1 : 2 : 17);
-editor.angularSliceX(1 : 2 : 17);
-
-%%
+% editor.angularSliceY(1 : 2 : 17);
+% editor.angularSliceX(1 : 2 : 17);
+editor.angularSliceY(1 : 9);
+editor.angularSliceX(1 : 9);
 
 editor.distanceBetweenTwoCameras = [0.03, 0.03];
 editor.cameraPlaneZ = 10;
@@ -15,8 +15,10 @@ editor.sensorSize = [1, 1];
 % editor.cameraPlaneZ = 500;
 % editor.sensorSize = [150, 200];
 
-
 lightField = editor.getPerspectiveLightField();
+
+%%
+
 numberOfLayers = 3;
 attenuatorThickness = 1;
 layerResolution = round( 1 * lightField.spatialResolution );
@@ -34,14 +36,14 @@ reconstructionIndices = [1, 1; 2, 2; 3, 3; 4, 4; 5, 5; 6, 6; 7, 7; 8, 8; 9, 9];
 rec.reconstructLightField();
 rec.evaluation.evaluateViews(reconstructionIndices);
 rec.evaluation.displayReconstructedViews();
-
+rec.evaluation.displayErrorImages();
 
 %% CSF LIGHT FIELD
 
 editor = LightFieldEditor();
 % editor.inputFromImageCollection('../lightFields/dice/perspective/3x3-.2_rect/', 'png', [3, 3], 1);
 % editor.inputFromImageCollection('../lightFields/legotruck/', 'png', [17, 17], 0.3);
-editor.inputFromImageCollection('../lightFields/CSF/', 'png', [9, 9], 0.7);
+editor.inputFromImageCollection('../lightFields/CSF/', 'png', [9, 9], 0.8);
 
 editor.distanceBetweenTwoCameras = [0.1, 0.1];
 editor.cameraPlaneZ = 10;
@@ -71,4 +73,47 @@ rec.evaluation.displayReconstructedViews();
 rec.evaluation.storeReconstructedViews('output/');
 rec.evaluation.displayErrorImages();
 rec.evaluation.storeErrorImages('output/');
+
+
+
+
+
+
+
+
+
+%% New version: sampling plane
+
+editor = LightFieldEditor();
+editor.inputFromImageCollection('../lightFields/CSF/', 'png', [9, 9], 0.1);
+editor.distanceBetweenTwoCameras = [0.1, 0.1];
+editor.cameraPlaneZ = 10;
+editor.sensorSize = [1, 1];
+editor.sensorPlaneZ = 0;
+
+lightField = editor.getPerspectiveLightField();
+
+%%
+numberOfLayers = 3;
+attenuatorThickness = numberOfLayers-1; % spacing is one
+layerResolution = round( 1 * lightField.spatialResolution );
+attenuator = Attenuator(numberOfLayers, layerResolution, [1.2, 1.2], attenuatorThickness / (numberOfLayers - 1), lightField.channels);
+
+resamplingPlane = SensorPlane(layerResolution, [1.1, 1.1], 0);
+
+rec = ReconstructionForResampledLF_V2(lightField, attenuator, resamplingPlane);
+rec.computeAttenuationLayers();
+
+
+close all;
+% printLayers(permute(attenuator.attenuationValues, [2, 3, 1, 4]), [10, 10], 'output/', 'print1', 1);
+
+% Indices of views for reconstruction and error evaluation
+reconstructionIndices = [1, 1; 2, 2; 3, 3; 4, 4; 5, 5; 6, 6; 7, 7; 8, 8; 9, 9];
+rec.reconstructLightField();
+rec.evaluation.evaluateViews(reconstructionIndices);
+rec.evaluation.displayReconstructedViews();
+% rec.evaluation.storeReconstructedViews('output/');
+% rec.evaluation.displayErrorImages();
+% rec.evaluation.storeErrorImages('output/');
 
