03406fafa57beedf4bc2623f1bf9489c06d62978
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-07-08 16:56:23 +0200

    sync

--- OOP/run.m
+++ OOP/run.m
@@ -3,10 +3,10 @@ editor = LightFieldEditor();
 % editor.inputFromImageCollection('../lightFields/dice/perspective/3x3-.2_rect/', 'png', [3, 3], 1);
 editor.inputFromImageCollection('../lightFields/legotruck/', 'png', [17, 17], 0.3);
 % editor.inputFromImageCollection('../lightFields/tarot/small_angular_extent/', 'png', [17, 17], 0.2);
-% editor.angularSliceY(1 : 2 : 17);
-% editor.angularSliceX(1 : 2 : 17);
-editor.angularSliceY(1 : 9);
-editor.angularSliceX(1 : 9);
+editor.angularSliceY(1 : 2 : 17);
+editor.angularSliceX(1 : 2 : 17);
+% editor.angularSliceY(1 : 9);
+% editor.angularSliceX(1 : 9);
 
 editor.distanceBetweenTwoCameras = [0.03, 0.03];
 editor.cameraPlaneZ = 10;
@@ -85,11 +85,15 @@ rec.evaluation.storeErrorImages('output/');
 %% New version: sampling plane
 
 editor = LightFieldEditor();
-editor.inputFromImageCollection('../lightFields/CSF/', 'png', [9, 9], 0.1);
+editor.inputFromImageCollection('../lightFields/CSF/', 'png', [9, 9], 0.8);
 editor.distanceBetweenTwoCameras = [0.1, 0.1];
 editor.cameraPlaneZ = 10;
 editor.sensorSize = [1, 1];
 editor.sensorPlaneZ = 0;
+
+% 2D 
+editor.angularSliceY(1);
+editor.spatialSliceY(1);
 
 lightField = editor.getPerspectiveLightField();
 
@@ -97,16 +101,65 @@ lightField = editor.getPerspectiveLightField();
 numberOfLayers = 3;
 attenuatorThickness = numberOfLayers-1; % spacing is one
 layerResolution = round( 1 * lightField.spatialResolution );
-attenuator = Attenuator(numberOfLayers, layerResolution, [1.2, 1.2], attenuatorThickness / (numberOfLayers - 1), lightField.channels);
+layerResolution(1) = 1;
+attenuator = Attenuator(numberOfLayers, layerResolution, [1, 1], attenuatorThickness / (numberOfLayers - 1), lightField.channels);
 
-resamplingPlane = SensorPlane(layerResolution, [1.1, 1.1], 0);
+resamplingPlane = SensorPlane(layerResolution, [1, 1], -1);
+
+rec = ReconstructionForResampledLF_V2(lightField, attenuator, resamplingPlane);
+rec.computeAttenuationLayers();
+
+
+% close all;
+printLayers(permute(attenuator.attenuationValues, [2, 3, 1, 4]), [10, 10], 'output/', 'print1', 1);
+
+% Indices of views for reconstruction and error evaluation
+reconstructionIndices = [1, 1; 2, 2; 3, 3; 4, 4; 5, 5; 6, 6; 7, 7; 8, 8; 9, 9];
+rec.reconstructLightField();
+rec.evaluation.evaluateViews(reconstructionIndices);
+rec.evaluation.displayReconstructedViews();
+rec.evaluation.storeReconstructedViews('output/');
+rec.evaluation.displayErrorImages();
+rec.evaluation.storeErrorImages('output/');
+
+
+
+%% Legotruck with SAMPLING PLANE
+
+editor = LightFieldEditor();
+% editor.inputFromImageCollection('../lightFields/dice/perspective/3x3-.2_rect/', 'png', [3, 3], 1);
+editor.inputFromImageCollection('../lightFields/legotruck/', 'png', [17, 17], 0.3);
+% editor.inputFromImageCollection('../lightFields/tarot/small_angular_extent/', 'png', [17, 17], 0.2);
+editor.angularSliceY(1 : 2 : 17);
+editor.angularSliceX(1 : 2 : 17);
+% editor.angularSliceY(1 : 9);
+% editor.angularSliceX(1 : 9);
+editor.distanceBetweenTwoCameras = [0.03, 0.03];
+editor.cameraPlaneZ = 10;
+editor.sensorSize = [1, 1];
+editor.sensorPlaneZ = 0;
+% editor.distanceBetweenTwoCameras = [8, 8];
+% editor.cameraPlaneZ = 500;
+% editor.sensorSize = [150, 200];
+
+lightField = editor.getPerspectiveLightField();
+
+%%
+
+numberOfLayers = 5;
+attenuatorThickness = numberOfLayers-1;
+layerResolution = round( 1.2 * lightField.spatialResolution );
+attenuator = Attenuator(numberOfLayers, layerResolution, [1, 1], attenuatorThickness / (numberOfLayers - 1), lightField.channels);
+
+
+resamplingPlane = SensorPlane(layerResolution, [1, 1], 0);
 
 rec = ReconstructionForResampledLF_V2(lightField, attenuator, resamplingPlane);
 rec.computeAttenuationLayers();
 
 
 close all;
-% printLayers(permute(attenuator.attenuationValues, [2, 3, 1, 4]), [10, 10], 'output/', 'print1', 1);
+printLayers(permute(attenuator.attenuationValues, [2, 3, 1, 4]), [10, 10], 'output/', 'print1', 1);
 
 % Indices of views for reconstruction and error evaluation
 reconstructionIndices = [1, 1; 2, 2; 3, 3; 4, 4; 5, 5; 6, 6; 7, 7; 8, 8; 9, 9];
@@ -116,4 +169,3 @@ rec.evaluation.displayReconstructedViews();
 % rec.evaluation.storeReconstructedViews('output/');
 % rec.evaluation.displayErrorImages();
 % rec.evaluation.storeErrorImages('output/');
-
