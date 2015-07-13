5a3e67b731e2a1838e519a8c253da1876a34c32b
awaelchli <adrian.waelchli@students.unibe.ch>
2015-07-13 17:40:21 +0200

    property: aspect ratio

--- OOP/LightField/LightField.m
+++ OOP/LightField/LightField.m
@@ -16,6 +16,8 @@ classdef LightField < handle
         angularResolution;
         spatialResolution;
         channels;
+        spatialAspectRatio;
+        angularAspectRatio;
     end
     
     methods
@@ -40,6 +42,14 @@ classdef LightField < handle
         
         function channels = get.channels(this)
             channels = size(this.lightFieldData, LightField.channelDimension);
+        end
+        
+        function aspect = get.spatialAspectRatio(this)
+            aspect = this.spatialResolution(2) / this.spatialResolution(1);
+        end
+        
+        function aspect = get.angularAspectRatio(this)
+            aspect = this.angularResolution(2) / this.angularResolution(1);
         end
         
         function replaceView(this, angularIndexY, angularIndexX, image)
