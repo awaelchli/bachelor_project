b59cde5351f2cb50abfb4eb016ddc9d6587c57ac
awaelchli <adrian.waelchli@students.unibe.ch>
2015-07-13 22:29:01 +0200

    comparing to old pipeline

--- OOP/runO.m
+++ OOP/runO.m
@@ -1,8 +1,9 @@
 editor = LightFieldEditor();
 editor.inputFromImageCollection('../lightFields/dice/orthographic/7x7x384x512_fov10/', 'png', [7, 7], 1);
+editor.lightFieldFOV = [10, 10];
 lightField = editor.getOrthographicLightField();
 
-attenuator = Attenuator(5, lightField.spatialResolution, [1, 1], 0.3, lightField.channels);
+attenuator = Attenuator(5, lightField.spatialResolution, [1, 1], 0.1, lightField.channels);
 
 rec = ReconstructionForOrthographicLF(lightField, attenuator);
 rec.computeAttenuationLayers();
@@ -14,3 +15,4 @@ rec.reconstructLightField();
 rec.evaluation.evaluateViews([4, 1; 4, 2; 4, 3; 4, 4; 4, 5; 4, 6; 4, 7]);
 rec.evaluation.displayReconstructedViews();
 rec.evaluation.displayErrorImages();
+rec.evaluation.storeReconstructedViews();
