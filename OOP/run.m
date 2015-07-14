c51f04d98d0e434ff896f44a04939fa28fd6bb87
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-07-14 17:42:51 +0200

    sync

--- OOP/run.m
+++ OOP/run.m
@@ -141,8 +141,8 @@ rec.evaluation.displayErrorImages();
 
 editor = LightFieldEditor();
 % editor.inputFromImageCollection('../lightFields/dice/perspective/3x3-.2_rect/', 'png', [3, 3], 1);
-editor.inputFromImageCollection('../lightFields/legotruck/', 'png', [17, 17], 0.2);
-% editor.inputFromImageCollection('../lightFields/tarot/small_angular_extent/', 'png', [17, 17], 0.2);
+% editor.inputFromImageCollection('../lightFields/legotruck/', 'png', [17, 17], 0.2);
+editor.inputFromImageCollection('../lightFields/tarot/small_angular_extent/', 'png', [17, 17], 0.2);
 editor.angularSliceY(1 : 3 : 17);
 editor.angularSliceX(1 : 3 : 17);
 % editor.angularSliceY(1 : 9);
@@ -173,46 +173,47 @@ lightFieldBlurred = LightFieldP(blurred, lightField.cameraPlane, lightField.sens
 %%
 
 numberOfLayers = 5;
-attenuatorThickness = 5;
-layerResolution = round( 1 * lightFieldBlurred.spatialResolution );
+attenuatorThickness = 4;
+layerResolution = round( 1 * lightField.spatialResolution );
 attenuator = Attenuator(numberOfLayers, layerResolution, [1, 1], attenuatorThickness / (numberOfLayers - 1), lightFieldBlurred.channels);
 
 
-resamplingPlane = SensorPlane(2 * layerResolution, [1, 1], 0);
+resamplingPlane = SensorPlane(1 * layerResolution, [1, 1], - attenuatorThickness / 2);
 
-rec = ReconstructionForResampledLF_V2(lightFieldBlurred, attenuator, resamplingPlane);
+rec = ReconstructionForResampledLF_V2(lightField, attenuator, resamplingPlane);
 rec.computeAttenuationLayers();
 
-resamplingPlane = SensorPlane(1 * layerResolution, [1, 1], 0);
-rec2 = ReconstructionForResampledLF_V2(lightFieldBlurred, attenuator, resamplingPlane);
+resamplingPlane2 = SensorPlane(1 * layerResolution, [1, 1], 0);
+rec2 = ReconstructionForResampledLF_V2(lightField, attenuator, resamplingPlane2);
 rec2.computeAttenuationLayers();
 
 rec.usePropagationMatrixForReconstruction(rec2.propagationMatrix);
 close all;
 
 rec.evaluation.displayLayers(1 : numberOfLayers);
+rec.evaluation.clearOutputFolder();
 rec.evaluation.storeLayers(1 : numberOfLayers);
 
 % Indices of views for reconstruction and error evaluation
 reconstructionIndices = [1, 1; 2, 2; 3, 3; 4, 4; 5, 5; 6, 6; 7, 7; 8, 8; 9, 9];
 rec.reconstructLightField();
-rec.evaluation.clearOutputFolder();
+
 rec.evaluation.evaluateViews(reconstructionIndices);
-rec.evaluation.displayReconstructedViews();
+% rec.evaluation.displayReconstructedViews();
 rec.evaluation.storeReconstructedViews();
-rec.evaluation.displayErrorImages();
+% rec.evaluation.displayErrorImages();
 rec.evaluation.storeErrorImages();
 
 
 %% Back projection P^T * LF
-P = rec.propagationMatrix.formSparseMatrix();
+P = rec.propagationMatrixForReconstruction.formSparseMatrix();
 l = reshape(rec.reconstructedLightField.lightFieldData, [], lightFieldBlurred.channels);
 backProjection = P' * l;
-b = backProjection ./ max(backProjection(:));
+b = backProjection ./ repmat(sum(P', 2), [1 3]);
 b = permute(b, [2, 1]);
 b = reshape(b, [attenuator.channels, attenuator.planeResolution, attenuator.numberOfLayers]);
 b = permute(b, [4, 2, 3, 1]);
-% b = exp(b);
+
 figure; imshow(squeeze(b(1, :, :, :)));
 figure; imshow(squeeze(b(2, :, :, :)));
 figure; imshow(squeeze(b(3, :, :, :)));
--- OOP/spectralSupport.m
+++ OOP/spectralSupport.m
@@ -1,17 +1,21 @@
 
-numberOfLightFields = 5;
-sensorPlaneZValues = -5 : 0.2 : 5;
+numberOfLightFields = 100;
+sensorPlaneZValues = 0;
 
 spatialResolution = [1, 100];
 angularResolution = [1, 15];
 
+thickness = 3;
+N = 8;
 cameraPlane = CameraPlane(angularResolution, [0.1, 0.1], 10);
-attenuator = Attenuator(3, spatialResolution, [1, 1], 1, 3);
+attenuator = Attenuator(N, spatialResolution, [1, 1], thickness / (N-1), 3);
 
 
 
 recFourierImages = cell(numberOfLightFields, numel(sensorPlaneZValues));
 fourierImages = cell(numberOfLightFields, numel(sensorPlaneZValues));
+
+lengthLastOutput = 0;
 
 for n = 1 : numberOfLightFields
     
@@ -29,6 +33,7 @@ for n = 1 : numberOfLightFields
         lightField = LightFieldP(data, cameraPlane, sensorPlane);
 
         rec = ReconstructionForResampledLF_V2(lightField, attenuator, resamplingPlane);
+        rec.verbose = 0;
         rec.computeAttenuationLayers();
         rec.reconstructLightField();
         recLF = rec.reconstructedLightField.lightFieldData;
@@ -36,27 +41,38 @@ for n = 1 : numberOfLightFields
         
         recFourierImages{n, iz} = fft2(recLF);
         fourierImages{n, iz} = fft2(squeeze(lightField.lightFieldData(:, :, :, :, 1)));
-
+        
+        out = sprintf('%i', iz);
+        fprintf(repmat('\b', 1, lengthLastOutput));
+        fprintf(out);
+        lengthLastOutput = numel(out);
     end
+    lengthLastOutput = 0;
+    fprintf('\n');
 end
 
-recFourierImageStack = cat(3, recFourierImages{5, :});
+recFourierImageStack = cat(3, recFourierImages{1, :});
 recAverageFourierImage = fftshift(recFourierImageStack); 
 recAverageFourierImage = abs(recAverageFourierImage);
 recAverageFourierImage = log(recAverageFourierImage+1);
-recAverageFourierImage = mat2gray(recAverageFourierImage);
 recAverageFourierImage = mean(recAverageFourierImage, 3);       
 
 
-fourierImageStack = cat(3, fourierImages{5, :});
+fourierImageStack = cat(3, fourierImages{1, :});
 averageFourierImage = fftshift(fourierImageStack);        
 averageFourierImage = abs(averageFourierImage);
 averageFourierImage = log(averageFourierImage+1);
-averageFourierImage = mat2gray(averageFourierImage);
 averageFourierImage = mean(averageFourierImage, 3);
 
+NZrecAverageFourierImage = recAverageFourierImage / max(recAverageFourierImage(:));
+
+NZrecAverageFourierImage(NZrecAverageFourierImage <= 0.3) = 0;
+NZrecAverageFourierImage(NZrecAverageFourierImage > 0.3) = 1;
+
 figure;
-subplot(211);
+subplot(311);
 imshow(recAverageFourierImage, []);
-subplot(212);
+subplot(312);
+imshow(NZrecAverageFourierImage, []);
+subplot(313);
 imshow(averageFourierImage, []);
