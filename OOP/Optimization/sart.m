f12220a6d6b36eb04e16b6533a66a9b79ca59209
awaelchli <adrian.waelchli@students.unibe.ch>
2015-07-15 15:31:05 +0200

    cosmetic changes

--- OOP/Optimization/sart.m
+++ OOP/Optimization/sart.m
@@ -9,27 +9,27 @@ function layers = sart( P, l, x0, lb, ub, iterations )
 %   iterations:     The number of SART iterations to perform
 
 
-% The weights for the updates: 
-% Vector W contains the row-sums of P
-% Vector V contains the column-sums of P
-W = P * ones(size(x0));
-W(W ~= 0) = 1 ./ W(W ~= 0);
+    % The weights for the updates: 
+    % Vector W contains the row-sums of P
+    % Vector V contains the column-sums of P
+    W = P * ones(size(x0));
+    W(W ~= 0) = 1 ./ W(W ~= 0);
 
-V = P' * ones(size(W));
-V(V ~= 0) = 1 ./ V(V ~= 0);
+    V = P' * ones(size(W));
+    V(V ~= 0) = 1 ./ V(V ~= 0);
 
-% Start with initial guess
-layers = x0;
+    % Start with initial guess
+    layers = x0;
 
-for i = 1 : iterations
-    
-    % Perform SART update
-    layers = layers + V .* (P' * ( W .* (l - P * layers)));
-    
-    % Clamp to desired range
-    layers(layers < lb) = lb(layers < lb);
-    layers(layers > ub) = ub(layers > ub);
-end
+    for i = 1 : iterations
+
+        % Perform SART update
+        layers = layers + V .* (P' * ( W .* (l - P * layers)));
+
+        % Clamp to desired range
+        layers(layers < lb) = lb(layers < lb);
+        layers(layers > ub) = ub(layers > ub);
+    end
 
 end
 
