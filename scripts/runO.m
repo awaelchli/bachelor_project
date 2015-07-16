ee547f505ee7084efdf49e15853030f00e2f3f75
awaelchli <adrian.waelchli@students.unibe.ch>
2015-07-16 17:56:04 +0200

    sync

--- scripts/runO.m
+++ scripts/runO.m
@@ -13,6 +13,7 @@ close all;
 rec.evaluation.displayLayers(1 : 5);
 rec.reconstructLightField();
 
+rec.evaluation.clearOutputFolder();
 rec.evaluation.evaluateViews([4, 1; 4, 2; 4, 3; 4, 4; 4, 5; 4, 6; 4, 7]);
 rec.evaluation.displayReconstructedViews();
 rec.evaluation.displayErrorImages();
