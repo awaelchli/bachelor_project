cbb5903a760060a8e7cdf5371b157e4274b2d38d
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-07-22 16:40:53 +0200

    sync

--- scripts/tarot_small_angles.m
+++ scripts/tarot_small_angles.m
@@ -1,20 +1,22 @@
 clear all;
 
+actualLayerWidth = 100;% mm
+
 editor = LightFieldEditor();
 editor.inputFromImageCollection('lightFields/tarot/small_angular_extent/', 'png', [17, 17], 0.3);
 editor.angularSliceY(1 : 3 : 17);
 editor.angularSliceX(1 : 3 : 17);
-editor.distanceBetweenTwoCameras = [0.03, 0.03];
+editor.distanceBetweenTwoCameras = [4 * 0.36, 4 * 0.36];
 editor.cameraPlaneZ = 10;
-editor.sensorSize = [1, 1];
+editor.sensorSize = [32, 32];
 editor.sensorPlaneZ = 0;
 
 lightField = editor.getPerspectiveLightField();
 
 numberOfLayers = 5;
-attenuatorThickness = (numberOfLayers-1) * .6;
+attenuatorThickness = 2;
 layerResolution = round( 1.0 * lightField.spatialResolution );
-attenuator = Attenuator(numberOfLayers, layerResolution, [1, 1], attenuatorThickness, lightField.channels);
+attenuator = Attenuator(numberOfLayers, layerResolution, [32, 32], attenuatorThickness, lightField.channels);
 
 % attenuator.placeLayer(1, -3.2);
 % attenuator.placeLayer(2, -2.5);
@@ -24,13 +26,13 @@ attenuator = Attenuator(numberOfLayers, layerResolution, [1, 1], attenuatorThick
 % attenuator.placeLayer(6, 0.8);
 % attenuator.placeLayer(7, 1.6);
 
-attenuator.placeLayer(1, -3.2);
-attenuator.placeLayer(2, -1.6);
+attenuator.placeLayer(1, -1.6);
+attenuator.placeLayer(2, -0.8);
 attenuator.placeLayer(3, 0);
-attenuator.placeLayer(4, 1.6);
-attenuator.placeLayer(5, 3.2);
+attenuator.placeLayer(4, 0.8);
+attenuator.placeLayer(5, 1.3);
 
-resamplingPlane = SensorPlane(round(3 * layerResolution), [1, 1], attenuator.layerPositionZ(1));
+resamplingPlane = SensorPlane(round(2 * layerResolution), [32, 32], attenuator.layerPositionZ(1));
 rec = ReconstructionForResampledLF(lightField, attenuator, resamplingPlane);
 
 
@@ -59,11 +61,12 @@ end
 
 rec.computeAttenuationLayers();
 rec.evaluation.displayLayers(1 : attenuator.numberOfLayers);
+rec.evaluation.storeLayers(1 : attenuator.numberOfLayers);
 
 %% Render Lf images
 
 % For the reconstruction, use a propagation matrix that projects from the sensor plane instead of the sampling plane
-resamplingPlane2 = SensorPlane(1 * layerResolution, [1, 1], 0);
+resamplingPlane2 = SensorPlane(1 * layerResolution, [32, 32], 0);
 rec2 = ReconstructionForResampledLF(lightField, attenuator, resamplingPlane2);
 rec2.constructPropagationMatrix();
 
