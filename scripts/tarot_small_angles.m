e09cfce20e38ecb6ab07da246313c220f613898e
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-07-24 15:36:15 +0200

    fine adjustments

--- scripts/tarot_small_angles.m
+++ scripts/tarot_small_angles.m
@@ -7,13 +7,13 @@ attenuatorSize = [actualLayerHeight, actualLayerWidth];
 samplingPlaneSize = attenuatorSize;
 
 editor = LightFieldEditor();
-editor.inputFromImageCollection('lightFields/tarot/small_angular_extent/', 'png', [17, 17], 0.5);
+editor.inputFromImageCollection('lightFields/tarot/small_angular_extent/', 'png', [17, 17], 0.3);
 editor.angularSliceY(17 : -3 : 1);
 editor.angularSliceX(17 : -3 : 1);
 editor.distanceBetweenTwoCameras = [5.76, 5.76];
 editor.cameraPlaneZ = 80;
 editor.sensorSize = attenuatorSize;
-editor.sensorPlaneZ = 1;
+editor.sensorPlaneZ = -0.5;
 
 lightField = editor.getPerspectiveLightField();
 
@@ -21,22 +21,6 @@ numberOfLayers = 5;
 attenuatorThickness = actualThickness;
 layerResolution = round( 1.1 * lightField.spatialResolution );
 attenuator = Attenuator(numberOfLayers, layerResolution, 1.1 * attenuatorSize, attenuatorThickness, lightField.channels);
-
-attenuator.translateLayers(1);
-
-% attenuator.placeLayer(1, -3.2);
-% attenuator.placeLayer(2, -2.5);
-% attenuator.placeLayer(3, -1.6);
-% attenuator.placeLayer(4, -0.8);
-% attenuator.placeLayer(5, 0);
-% attenuator.placeLayer(6, 0.8);
-% attenuator.placeLayer(7, 1.6);
-
-% attenuator.placeLayer(1, -1.6);
-% attenuator.placeLayer(2, -0.8);
-% attenuator.placeLayer(3, 0);
-% attenuator.placeLayer(4, 0.8);
-% attenuator.placeLayer(5, 1.3);
 
 resamplingPlane = SensorPlane(round(2 * layerResolution), samplingPlaneSize, attenuator.layerPositionZ(1));
 rec = ReconstructionForResampledLF(lightField, attenuator, resamplingPlane);
@@ -74,7 +58,7 @@ rec.evaluation.storeLayers(1 : attenuator.numberOfLayers);
 %% Reconstruct light field from layers
 
 % For the reconstruction, use a propagation matrix that projects from the sensor plane instead of the sampling plane
-resamplingPlane2 = SensorPlane(round(1 * layerResolution), samplingPlaneSize, lightField.sensorPlane.z);
+resamplingPlane2 = SensorPlane(round(1.5 * layerResolution), samplingPlaneSize, lightField.sensorPlane.z);
 rec2 = ReconstructionForResampledLF(lightField, attenuator, resamplingPlane2);
 rec2.constructPropagationMatrix();
 
