75b16054fc976b52864ed603fb4f87c30671f945
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2015-09-17 22:22:03 +0200

    Exercise 2 solved

---
+++ JRTR/src/assignment1/Exercise2.java
@@ -0,0 +1,49 @@
+package assignment1;
+
+import java.io.IOException;
+
+import glWrapper.GLHalfedgeStructure;
+import meshes.HalfEdgeStructure;
+import meshes.WireframeMesh;
+import meshes.exception.DanglingTriangleException;
+import meshes.exception.MeshNotOrientedException;
+import meshes.reader.ObjReader;
+import openGL.MyDisplay;
+
+/**
+ * 
+ * @author Adrian Wälchli
+ *
+ */
+public class Exercise2 {
+
+	public static void main(String[] args) throws IOException{
+		// Load a wireframe mesh
+		WireframeMesh m = ObjReader.read("./objs/teapot.obj", true);
+		HalfEdgeStructure hs = new HalfEdgeStructure();
+		
+		/*
+		 * Create a half-edge structure out of the wireframe description.
+		 * As not every mesh can be represented as a half-edge structure
+		 * exceptions could occur.
+		 */
+		try {
+			hs.init(m);
+		} catch (MeshNotOrientedException | DanglingTriangleException e) {
+			e.printStackTrace();
+			return;
+		}
+		
+		MyDisplay display = new MyDisplay();
+		
+		GLHalfedgeStructure glTeapot = new GLHalfedgeStructure(hs);
+		glTeapot.configurePreferredShader("shaders/default.vert", "shaders/default.frag", null);
+		display.addToDisplay(glTeapot);
+		
+		// Do the same but choose a different shader
+		GLHalfedgeStructure glTeapot2 = new GLHalfedgeStructure(hs);
+		glTeapot2.configurePreferredShader("shaders/trimesh_flat.vert", "shaders/trimesh_flat.frag", "shaders/trimesh_flat.geom");
+		display.addToDisplay(glTeapot2);
+	}
+
+}
---
+++ JRTR/src/glWrapper/GLHalfedgeStructure.java
@@ -0,0 +1,81 @@
+package glWrapper;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import javax.media.opengl.GL;
+import javax.vecmath.Point3f;
+
+import meshes.Face;
+import meshes.HalfEdgeStructure;
+import meshes.Vertex;
+import openGL.gl.GLDisplayable;
+import openGL.gl.GLRenderer;
+import openGL.objects.Transformation;
+
+/**
+ * 
+ * @author Adrian Wälchli
+ *
+ */
+public class GLHalfedgeStructure extends GLDisplayable {
+
+	public GLHalfedgeStructure(HalfEdgeStructure structure) {
+		super(structure.getVertices().size());
+
+		/*
+		 * Add vertices
+		 */
+		Iterator<Vertex> vertexIterator = structure.iteratorV();
+		float[] glVertices = new float[3 * this.getNumberOfVertices()];
+
+		HashMap<Vertex, Integer> vertexIndexMap = new HashMap<Vertex, Integer>();
+
+		int c = 0;
+		int vertexIndex = 0;
+		while (vertexIterator.hasNext()) {
+
+			Vertex vertex = vertexIterator.next();
+			Point3f point = vertex.getPos();
+
+			glVertices[c++] = point.x;
+			glVertices[c++] = point.y;
+			glVertices[c++] = point.z;
+
+			vertexIndexMap.put(vertex, vertexIndex++);
+		}
+
+		this.addElement(glVertices, Semantic.POSITION, 3);
+		this.addElement(glVertices, Semantic.USERSPECIFIED , 3, "color");
+
+		/*
+		 * Create indices
+		 */
+		int[] glIndices = new int[3 * structure.getFaces().size()];
+
+		Iterator<Face> faceIterator = structure.iteratorF();
+		c = 0;
+		while (faceIterator.hasNext()) {
+
+			Iterator<Vertex> faceVertexIterator = faceIterator.next().iteratorFV();
+			while (faceVertexIterator.hasNext()) {
+				Vertex vertex = faceVertexIterator.next();
+				int index = vertexIndexMap.get(vertex);
+				glIndices[c++] = index;
+			}
+
+		}
+		this.addIndices(glIndices);
+	}
+
+	@Override
+	public int glRenderFlag() {
+		return GL.GL_TRIANGLES;
+	}
+
+	@Override
+	public void loadAdditionalUniforms(GLRenderer glRenderContext, Transformation mvMat) {
+		// To be implemented
+	}
+
+}
