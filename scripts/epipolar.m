7e4f35a9dce2659ff7c53f636e8663890d958a72
awaelchli <adrian.waelchli@students.unibe.ch>
2015-11-03 22:47:24 +0100

    support for large light fields

--- scripts/epipolar.m
+++ scripts/epipolar.m
@@ -1,28 +1,33 @@
-% close all;
+inputFolder = 'lightFields/dice/perspective/1x500x1000x1000/';
+fileType = 'png';
+angularResolution = [1 500];
+epiOutput = 'output/epi_1x500x1000x1000/';
 
+% Prepare to write results in output folder
+if ~exist(epiOutput, 'dir')
+    mkdir(epiOutput);
+end
+
+% Load middle image
 editor = LightFieldEditor();
-editor.inputFromImageCollection('lightFields/dice/perspective/1x100x1000x1000/', 'png', [1, 100], 1);
+editor.inputFromImageCollection(inputFolder, fileType, angularResolution, 1);
+editor.angularSliceX([1, angularResolution(2)]);
 
 LF = editor.getPerspectiveLightField();
-data = LF.lightFieldData;
+left = squeeze(LF.lightFieldData(1, 1, :, :, :));
+right = squeeze(LF.lightFieldData(1, 2, :, :, :));
+spatialResolution = size(left);
 
-spatialResolution = LF.spatialResolution;
-angularResolution = LF.angularResolution;
-
-fixedY = 411;
-
-left = squeeze(data(1, floor(LF.angularResolution(2) / 2), :, :, :));
-epi = squeeze(data(:, :, fixedY, :, :));
+overview = (left + 0.1 * right);
+overview = (overview - min(overview(:))) / (max(overview(:)) - min(overview(:))); 
 
 figure(1);
 subplot(1, 2, 1);
-imagesc(left);
-hold on;
-plot([1 spatialResolution(2)], [fixedY, fixedY], 'Color', 'black');
+imagesc(overview);
+title('Left- and Rightmost Views');
 axis equal tight;
-subplot(1, 2, 2);
-imagesc(epi);
-axis equal tight;
+
+
 
 while true
     
@@ -34,17 +39,28 @@ while true
     
     y = max(floor(y), 1);
     
-    epi = squeeze(data(:, :, y, :, :));
+    % Draw line in overview image
+    overview(y, :, :) = 0;
+    
+    % Load slice of selected scanline
+    editor.inputFromImageCollection(inputFolder, fileType, angularResolution, 1);
+    editor.spatialSliceY(y);
+    LF = editor.getPerspectiveLightField();
+    epi = squeeze(LF.lightFieldData);
+    
+    % Write epipolar image to file
+    imwrite(epi, sprintf([epiOutput 'scanY=%i.%s'], y, fileType));
     
     figure(1);
     subplot(1, 2, 1);
-    imagesc(left);
-    hold on;
-    plot([1 spatialResolution(2)], [y, y], 'Color', 'black');
+    imagesc(overview);
+    title('Left- and Rightmost Views');
     axis equal tight;
-    hold off;
     
     subplot(1, 2, 2);
     imagesc(epi);
+    title('Epipolar Image');
     axis equal tight;
 end
+
+imwrite(overview, [epiOutput 'overview.' fileType]);
