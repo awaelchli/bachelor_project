dcbdbf9281ab92d5ab478a63015d9426619230ae
awaelchli <adrian.waelchli@students.unibe.ch>
2016-01-01 23:12:43 +0100

    trying to fix

--- scripts/spectralSupport.m
+++ scripts/spectralSupport.m
@@ -1,10 +1,10 @@
 
 numberOfLightFields = 100;
-spatialResolution = [1, 100];
+spatialResolution = [100, 100];
 angularResolution = [1, 15];
 thickness = 3;
 N = 8;
-attenuator = Attenuator(N, [1, 1 * spatialResolution(2)], [1, 1], thickness, 3);
+attenuator = Attenuator(N, spatialResolution, [1, 1], thickness, 3);
 resamplingPlaneZ = -thickness / 2;
 cameraPlane = CameraPlane(angularResolution, [0.1, 0.1], 10);
 sensorPlane = SensorPlane(spatialResolution, attenuator.planeSize, 0);
@@ -17,22 +17,22 @@ lengthLastOutput = 0;
 % Precompute projection matrix for reconstruction
 tempResamplingPlane = SensorPlane(spatialResolution, attenuator.planeSize, 0);
 tempLightField = LightFieldP(zeros([angularResolution, spatialResolution, 3]), cameraPlane, sensorPlane);
-tempRec = ReconstructionForResampledLF_V2(tempLightField, attenuator, tempResamplingPlane);
+tempRec = FastReconstructionForResampledLF(tempLightField, attenuator, tempResamplingPlane);
 tempRec.verbose = 0;
-tempRec.computeAttenuationLayers(); % Not actually needed, TODO: remove
+%tempRec.computeAttenuationLayers(); % Not actually needed, TODO: remove
 P = tempRec.propagationMatrix;
 
 for n = 1 : numberOfLightFields
     
-    data = randn(100, 100);
+    data = randn(spatialResolution);
     data = repmat(data, 1, 1, 15, 15, 3);
     data = permute(data, [3, 4, 1, 2, 5]);
-    data = data(1, :, 1, :, :);
+    data = data(1, :, :, :, :);
     
-    resamplingPlane = SensorPlane([1, 2 * spatialResolution(2)], attenuator.planeSize, resamplingPlaneZ);
+    resamplingPlane = SensorPlane(2 * spatialResolution, attenuator.planeSize, resamplingPlaneZ);
     lightField = LightFieldP(data, cameraPlane, sensorPlane);
 
-    rec = ReconstructionForResampledLF_V2(lightField, attenuator, resamplingPlane);
+    rec = FastReconstructionForResampledLF(lightField, attenuator, resamplingPlane);
     rec.verbose = 0;
     rec.computeAttenuationLayers();
     rec.usePropagationMatrixForReconstruction(P);
