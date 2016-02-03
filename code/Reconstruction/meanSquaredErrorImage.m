33e1be68e6055baf212c6532398b1011b695884f
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-02-04 00:43:57 +0100

    propper mse with color channels

--- code/Reconstruction/meanSquaredErrorImage.m
+++ code/Reconstruction/meanSquaredErrorImage.m
@@ -1,9 +1,11 @@
 function [ errorImage, rmse, psnr ] = meanSquaredErrorImage( image1, image2 )
 
+channels = size(image1, 3);
+
 errorImage = (255 * (image1 - image2)) .^ 2;
 errorImage = sum(errorImage, 3);
 
-mse = mean(errorImage(:));
+mse = mean(errorImage(:)) / channels;
 rmse = sqrt(mse);
 psnr = 10 * log10((255^2) / mse);
 
