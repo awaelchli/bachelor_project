77c05c960b14db2186cdb274daaec958ff7f42ec
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-02-21 23:08:40 +0100

    reconstruct with sensorsize rather than layersize

--- gui/gui_reconstruct_lightfield.m
+++ gui/gui_reconstruct_lightfield.m
@@ -1,12 +1,14 @@
 function [ evaluation ] = gui_reconstruct_lightfield( handles )
 
 
-layerResolution = handles.data.attenuator.planeResolution;
-attenuatorSize = handles.data.attenuator.planeSize;
+% layerResolution = handles.data.attenuator.planeResolution;
+% attenuatorSize = handles.data.attenuator.planeSize;
+sensorSize = handles.data.lightfield.sensorPlane.planeSize;
+sensorResolution = handles.data.lightfield.sensorPlane.planeResolution;
 z = handles.data.lightfield.sensorPlane.z;
 
 % For the reconstruction, use a propagation matrix that projects from the sensor plane instead of the sampling plane
-resamplingPlane = SensorPlane(round(1 * layerResolution), round(1 * attenuatorSize), z);
+resamplingPlane = SensorPlane(round(1 * sensorResolution), round(1 * sensorSize), z);
 
 switch get(handles.popupProjectionType, 'Value')
     case 1
