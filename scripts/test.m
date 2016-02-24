a86dd44ace7a3b1565a556b7b07bc3a696d6d893
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-02-24 23:28:04 +0100

    produce csf plots

--- scripts/test.m
+++ scripts/test.m
@@ -33,3 +33,63 @@ for cy = 1 : resolution(LightField.angularDimensions(1))
         
     end
 end
+
+%% CSF plots
+load('C:\Users\Adrian\Documents\MATLAB\bachelor_project\results\contrast\perspective\2layer\evaluation.mat');
+evaluation2 = evaluation;
+load('C:\Users\Adrian\Documents\MATLAB\bachelor_project\results\contrast\perspective\5layer\evaluation.mat');
+evaluation5 = evaluation;
+load('C:\Users\Adrian\Documents\MATLAB\bachelor_project\results\contrast\perspective\5layer_1.5_resolution\evaluation.mat');
+evaluation15 = evaluation;
+
+err2 = evaluation2.displaySingleErrorImage([5,5]);
+err5 = evaluation5.displaySingleErrorImage([5,5]);
+err15 = evaluation15.displaySingleErrorImage([5,5]);
+
+max2 = max(err2(:))
+max5 = max(err5(:))
+max15 = max(err15(:))
+%%
+figure(1)
+ax1 = axes;
+imagesc(err2)
+caxis([0, 0.2])
+colormap jet
+colorbar('Ticks', [0, 0.2])
+axis off
+axis equal image
+set(ax1, 'XTickLabel','');
+set(ax1, 'YTickLabel','');
+
+s = 255 / max2;
+imwrite(s * err2, jet(255), '2layer.png');
+
+%%
+figure(2)
+ax1 = axes;
+imagesc(err5)
+caxis([0, 0.2])
+colormap jet
+colorbar('Ticks', [0, 0.2])
+axis off
+axis equal image
+set(ax1, 'XTickLabel','');
+set(ax1, 'YTickLabel','');
+
+s = 255 / max2;
+imwrite(s * err5, jet(255), '5layer.png');
+
+%%
+figure(3)
+ax1 = axes;
+imagesc(err15)
+caxis([0, 0.2])
+colormap jet
+colorbar('Ticks', [0, 0.2])
+axis off
+axis equal image
+set(ax1, 'XTickLabel','');
+set(ax1, 'YTickLabel','');
+
+s = 255 / max2;
+imwrite(s * err15, jet(255), '5layer_1.5x_resolution.png');
