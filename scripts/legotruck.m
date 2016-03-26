58231cff7a4f29f2a3540c6a5f1c8c47d1d2282a
Adrian Wälchli <adrian.waelchli@students.unibe.ch>
2016-03-26 14:17:28 +0100

    bilinear interp. for legotruck

--- scripts/legotruck.m
+++ scripts/legotruck.m
@@ -20,7 +20,7 @@ lightField = editor.getPerspectiveLightField();
 
 numberOfLayers = 5;
 attenuatorThickness = actualThickness;
-layerResolution = round( 3 * lightField.spatialResolution );
+layerResolution = round( 1 * lightField.spatialResolution );
 attenuator = Attenuator(numberOfLayers, layerResolution, attenuatorSize, attenuatorThickness, lightField.channels);
 
 resamplingPlane = SensorPlane(round(1 * layerResolution), attenuatorSize, attenuator.layerPositionZ(1));
@@ -43,13 +43,14 @@ end
 
 
 %% Compute the layers
-
+rec2.bilinearLayerInterpolation();
 rec.computeAttenuationLayers();
 rec.evaluation.displayLayers(1 : attenuator.numberOfLayers);
 
 % For the reconstruction, use a propagation matrix that projects from the sensor plane instead of the sampling plane
 resamplingPlane2 = SensorPlane(1 * layerResolution, attenuatorSize, lightField.sensorPlane.z);
 rec2 = FastReconstructionForResampledLF(lightField, attenuator, resamplingPlane2);
+rec2.nearestLayerInterpolation();
 rec2.constructPropagationMatrix();
 
 rec.usePropagationMatrixForReconstruction(rec2.propagationMatrix);
